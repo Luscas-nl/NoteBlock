6454ab1290234e5e03d7676380f0341737885328
Luscas-nl <eduardol123azq@gmail.com>
2022-05-18 22:22:57 -0300

    Dute

--- main.py
+++ main.py
@@ -4,6 +4,26 @@ from tkinter import font
 import os
 
 class MenuEditar():
+    def Configurations(self):
+        diretorios = os.listdir()
+        if "config.txt" in diretorios:
+            with open("config.txt", "r", encoding="utf-8") as w:
+                line = w.readlines()
+                self.fontFamily = line[0]
+                self.fontSize = line[1].strip()
+                self.themeBack = line[2].strip()
+                self.themeFore = line[3].strip()
+                self.themeName = line[4].strip()
+            print(f"Familia: {self.fontFamily[0:-1]}")
+            print(f"Tamanho Fonte: {self.fontSize}")
+            print(f"Cor Background: {self.themeBack}")
+            print(f"Cor Fonte: {self.themeFore}")
+            print(f"Nome do Tema: {self.themeName}")
+        else:
+            with open("config.txt", "w", encoding="utf-8") as w:
+                w.write(f"Lucida Console\n{'12'}\n{'#202020'}\n{'white'}\n{'Dark'}")
+                self.Configurations
+    
     
     def Theme(self):
         temaAtual = self.strTH.get().upper()
@@ -11,16 +31,44 @@ class MenuEditar():
         if temaAtual == "LIGHT":
             self.txt.configure(fg= "black", background= "white")
             self.fr.configure(background= "white")
+            with open("config.txt", "w", encoding="utf-8") as w:
+                w.write(f"{self.strFF.get()}\n{self.strFS.get()}\n{'white'}\n{'black'}\n{'Light'}")
 
+        elif temaAtual == "OCEAN":
+            self.txt.configure(fg= "#1f51bf", background= "#000c18")
+            self.fr.configure(background= "#000c18")
+            with open("config.txt", "w", encoding="utf-8") as w:
+                w.write(f"{self.strFF.get()}\n{self.strFS.get()}\n{'#000c18'}\n{'#1f51bf'}\n{'Ocean'}")
+            
+        elif temaAtual == "WOOD":
+            self.txt.configure(fg= "#c1b9ab", background= "#221a0f")
+            self.fr.configure(background= "#221a0f")
+            with open("config.txt", "w", encoding="utf-8") as w:
+                w.write(f"{self.strFF.get()}\n{self.strFS.get()}\n{'#221a0f'}\n{'#c1b9ab'}\n{'Wood'}")
+            
+        elif temaAtual == "ECLIPSE":
+            self.txt.configure(fg= "#a2341e", background= "#390000")
+            self.fr.configure(background= "#390000")
+            with open("config.txt", "w", encoding="utf-8") as w:
+                w.write(f"{self.strFF.get()}\n{self.strFS.get()}\n{'#390000'}\n{'#a2341e'}\n{'Eclipse'}")
+            
         else:
             self.txt.configure(fg= "white", background= "#202020")
             self.fr.configure(background= "#202020")
-    
+            with open("config.txt", "w", encoding="utf-8") as w:
+                w.write(f"{self.strFF.get()}\n{self.strFS.get()}\n{'#202020'}\n{'#white'}\n{'Dark'}")
+
+        self.Configurations()
+        
+
     def FontStile(self):
         fontS = font.Font(self.txt, self.txt.cget("font"))
         fontS.configure(size= int(self.strFS.get()), family= self.strFF.get())
         self.txt.tag_configure("FontStile", font= fontS)
         self.txt.tag_add("FontStile", 1.0, END)
+        with open("config.txt", "w", encoding="utf-8") as w:
+            w.write(f"{self.strFF.get()}\n{self.strFS.get()}\n{self.themeBack}\n{self.themeFore}\n{self.themeName}")
+        self.Configurations()
         
     def Selecao(self):
         self.selecionado = False
@@ -70,46 +118,50 @@ class MenuEditar():
                 self.txt.tag_add("italic", "sel.first", "sel.last")
     
     def Opcoes(self): 
-        self.win3 = Tk()
-        self.win3.title("Preferências")
-        self.win3.config(background= "#202020")
-        self.win3.iconbitmap("image/duteblock_icon.ico")
-        
-        x = (self.screenW / 2) - (300 / 2)
-        y = (self.screenH / 2) - (125 / 2)
-        self.win3.geometry(f"300x125+{int(x)}+{int(y)}")
-        
-        self.frame03 = Frame(self.win3, background= "#202020", bd= 0)
-        self.frame03.place(relx=0, rely=0, relheight=1, relwidth=1)
-        
-        self.lb03 = Label(self.frame03, text= "Font Family", background= "#202020", fg= "#434343", font= ("Montsserat", 15))
-        self.lb03.place(relx= 0.05, rely= 0.15, relheight= 0.2, relwidth= 0.35)
-        self.lb04 = Label(self.frame03, text= "Font Size", background= "#202020", fg= "#434343", font= ("Montsserat", 15))
-        self.lb04.place(relx= 0.044, rely= 0.4, relheight= 0.2, relwidth= 0.3)
-        self.lb05 = Label(self.frame03, text= "Theme", background= "#202020", fg= "#434343", font= ("Montsserat", 15))
-        self.lb05.place(relx= 0.06, rely= 0.65, relheight= 0.2, relwidth= 0.2)
-        
-        self.strFS = StringVar(self.frame03)
-        self.strFS.set("12")
-        self.tipFS = ("8", "9", "10", "11", "12", "14", "16", "18", "20", "22", "24", "26", "28", "36", "48", "72")
-        self.optFS = OptionMenu(self.frame03, self.strFS, *self.tipFS, command= lambda x: self.FontStile())
-        self.optFS.config(bg="#111111", font= ("Montsserat", 8), fg= "white", bd= 0, highlightthickness= 0.5, highlightbackground= "#434343", )
-        self.optFS.place(relx= 0.35, rely= 0.4, relwidth= 0.59, relheight= 0.2)
-        
-        self.strFF = StringVar(self.frame03)
-        self.strFF.set("Lucida Console")
-        self.tipFF = ("Arial", "Lucida Console", "Montsserat", "Times New Roman")
-        self.optFF = OptionMenu(self.frame03, self.strFF, *self.tipFF, command= lambda x: self.FontStile())
-        self.optFF.config(bg="#111111", font= ("Montsserat", 8), fg= "white", bd= 0, highlightthickness= 0.5, highlightbackground= "#434343", )
-        self.optFF.place(relx= 0.42, rely= 0.15, relwidth= 0.52, relheight= 0.2)
-        
-        self.strTH = StringVar(self.frame03)
-        self.strTH.set("Dark")
-        self.tipTH = ("Dark", "Light")
-        self.optTH = OptionMenu(self.frame03, self.strTH, *self.tipTH, command= lambda x: self.Theme())
-        self.optTH.config(bg="#111111", font= ("Montsserat", 8), fg= "white", bd= 0, highlightthickness= 0.5, highlightbackground= "#434343", )
-        self.optTH.place(relx= 0.28, rely= 0.65, relwidth= 0.66, relheight= 0.2)
-        self.win3.mainloop()
+        if self.win3 is None:
+            self.win3 = Tk()
+            self.win3.title("Preferências")
+            self.win3.config(background= "#202020")
+            self.win3.iconbitmap("image/duteblock_icon.ico")
+            
+            x = (self.screenW / 2) - (300 / 2)
+            y = (self.screenH / 2) - (125 / 2)
+            self.win3.geometry(f"300x125+{int(x)}+{int(y)}")
+            
+            self.frame03 = Frame(self.win3, background= self.themeBack, bd= 0)
+            self.frame03.place(relx=0, rely=0, relheight=1, relwidth=1)
+            
+            self.lb03 = Label(self.frame03, text= "Font Family", background= self.themeBack, fg= self.themeFore, font= ("Arial Black", 12))
+            self.lb03.place(relx= 0.05, rely= 0.15, relheight= 0.2, relwidth= 0.35)
+            self.lb04 = Label(self.frame03, text= "Font Size", background= self.themeBack, fg= self.themeFore, font= ("Arial Black", 12))
+            self.lb04.place(relx= 0.04, rely= 0.4, relheight= 0.2, relwidth= 0.3)
+            self.lb05 = Label(self.frame03, text= "Theme", background= self.themeBack, fg= self.themeFore, font= ("Arial Black", 12))
+            self.lb05.place(relx= 0.05, rely= 0.65, relheight= 0.2, relwidth= 0.2)
+            
+            self.strFS = StringVar(self.frame03)
+            self.strFS.set(self.fontSize)
+            self.tipFS = ("8", "9", "10", "11", "12", "14", "16", "18", "20", "22", "24", "26", "28", "36", "48", "72")
+            self.optFS = OptionMenu(self.frame03, self.strFS, *self.tipFS, command= lambda x: self.FontStile())
+            self.optFS.config(bg="#111111", font= ("Lucida Console", 8), fg= "white", bd= 0, highlightthickness= 0.5,
+                            highlightbackground= "#434343", activebackground= "#202020", activeforeground= "white")
+            self.optFS.place(relx= 0.35, rely= 0.4, relwidth= 0.59, relheight= 0.2)
+            
+            self.strFF = StringVar(self.frame03)
+            self.strFF.set(self.fontFamily[0:-1])
+            self.tipFF = ("Arial", "Arial Black", "Arial Narrow", "Lucida Console", "Times New Roman")
+            self.optFF = OptionMenu(self.frame03, self.strFF, *self.tipFF, command= lambda x: self.FontStile())
+            self.optFF.config(bg="#111111", font= ("Arial Condensed", 8), fg= "white", bd= 0, highlightthickness= 0.5,
+                            highlightbackground= "#434343", activebackground= "#202020", activeforeground= "white")
+            self.optFF.place(relx= 0.42, rely= 0.15, relwidth= 0.52, relheight= 0.2)
+            
+            self.strTH = StringVar(self.frame03)
+            self.strTH.set(self.themeName)
+            self.tipTH = ("Dark", "Light", "Ocean", "Wood", "Eclipse")
+            self.optTH = OptionMenu(self.frame03, self.strTH, *self.tipTH, command= lambda x: self.Theme())
+            self.optTH.config(bg="#111111", font= ("Arial Condensed", 8), fg= "white", bd= 0, highlightthickness= 0.5,
+                            highlightbackground= "#434343", activebackground= "#202020", activeforeground= "white")
+            self.optTH.place(relx= 0.28, rely= 0.65, relwidth= 0.66, relheight= 0.2)
+            self.win3.mainloop()
 
 class MenuArquivo():
     def ConfirmarAbrir(self):
@@ -268,7 +320,9 @@ class MenuArquivo():
 class Application(MenuArquivo, MenuEditar):
     def __init__(self):
         self.win1 = Tk()
+        self.win3 = None
         self.dark = True
+        self.Configurations()
         self.Config()
         self.Frames1()
         self.Widgets1()
@@ -289,11 +343,11 @@ class Application(MenuArquivo, MenuEditar):
         self.win1.resizable(True, True)                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               
         
     def Frames1(self):
-        self.fr = Frame(self.win1, background= "#202020")
+        self.fr = Frame(self.win1, background= self.themeBack)
         self.fr.place(relx= 0, rely= 0, relwidth= 1, relheight= 1)
         
     def Widgets1(self):
-        self.txt = Text(self.fr, background= "#202020", undo= True, fg= "white", font= ("Lucida Console", 12), border=0, wrap= WORD)
+        self.txt = Text(self.fr, background= self.themeBack, undo= True, fg= self.themeFore, font= (self.fontFamily[0:-1], int(self.fontSize)), border=0, wrap= WORD)
         self.txt.place(relx= 0.01, rely= 0.02, relwidth= 0.98, relheight= 0.96)
         
     def Menu1(self):
